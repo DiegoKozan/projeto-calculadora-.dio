da98ab686b12429232f9b2205b356842f8ba9e54
Diego Ferreira kozan <113187839+DiegoKozan@users.noreply.github.com>
2023-04-07 10:18:48 -0300

    Add files via upload
    estudo de comportamento de estrutura  em Python

---
+++ project_aula_1_calculadora.py
@@ -0,0 +1,12 @@
+#Qual o resultado na variável operacao_1: 25 ou 17?
+operacao_1 = 2 + 3 * 5
+#qual o resultado armezenado na opercao_2: 25 ou 17?
+operacao_2 = (2+3)*5
+#qual o resultado armazenado na variável operacao_3: 4 ou 1 ?
+operacao_3 = 4 /  2 ** 2
+#qual o resltado armazenado na vaeiável operacao4: 1ou 5?
+operacao_4 = 13 % 3 + 4
+print(f'Resultado em operacao_1 = {operacao_1}')
+print(f'resultado em operacao_2 = {operacao_2}')
+print(f'resultado em operacao_3 = {operacao_3}')
+print(f'resultado em operacao_4 = {operacao_4}')
